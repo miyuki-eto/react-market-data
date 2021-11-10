a0e468a917c721dee5a1a78bb445d91cdf6b3368
Miyuki Eto <miyukietocrypt@gmail.com>
2021-11-10 16:22:57 +1000

    add coin margin

--- src/pages/openInterestAll.js
+++ src/pages/openInterestAll.js
@@ -11,10 +11,17 @@ axiosThrottle.use(axios, {requestsPerSecond: 9});
 
 export default function OpenInterestAll() {
     const [binanceTokens, setBinanceTokens] = useState([]);
+    const [binanceTokensCoin, setBinanceTokensCoin] = useState([]);
+    const [binanceTokensCoinType, setBinanceTokensCoinType] = useState([]);
     const [weights, setWeights] = useState(() => {
         return JSON.parse(localStorage.getItem("tokenBool")) || [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     });
+    const [weightsCoin, setWeightsCoin] = useState(() => {
+        return JSON.parse(localStorage.getItem("tokenCoinBool")) || [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    });
+
     const [binanceOi, setBinanceOi] = useState([]);
+    const [binanceOiCoin, setBinanceOiCoin] = useState([]);
     const [binanceOiCombined, setBinanceOiCombined] = useState([]);
     const [loading, setLoading] = useState(true);
     const [timeframe, setTimeframe] = useState(() => {
@@ -23,12 +30,22 @@ export default function OpenInterestAll() {
 
     function updateWeights(index) {
         let weightsCopy = [...weights];
-        if (weightsCopy[index] === 1){
+        if (weightsCopy[index] === 1) {
             weightsCopy[index] = 0;
         } else {
             weightsCopy[index] = 1;
         }
         setWeights(weightsCopy);
+    }
+
+    function updateWeightsCoin(index) {
+        let weightsCopy = [...weightsCoin];
+        if (weightsCopy[index] === 1) {
+            weightsCopy[index] = 0;
+        } else {
+            weightsCopy[index] = 1;
+        }
+        setWeightsCoin(weightsCopy);
     }
 
     function initWeights(tokens) {
@@ -40,14 +57,26 @@ export default function OpenInterestAll() {
         setWeights(arr)
     }
 
+    function initWeightsCoin(tokens) {
+        const arr = [];
+        const len = tokens.length;
+        for (let i = 1; i <= len; i++) {
+            arr.push(1);
+        }
+        setWeightsCoin(arr)
+    }
+
     const binanceTickers = ["BTCUSDT", "ETHUSDT", "LINKUSDT", "UNIUSDT", "DOTUSDT", "SNXUSDT", "SUSHIUSDT", "BNBUSDT", "AAVEUSDT", "YFIUSDT", "MKRUSDT", "SOLUSDT", "LTCUSDT", "DOGEUSDT"];
 
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true)
             setBinanceOi([])
+            setBinanceOiCoin([])
             setBinanceTokens([])
-            getBinanceOiData(binanceTokens, timeframe)
+            setBinanceTokensCoin([])
+            setBinanceTokensCoinType([])
+            getBinanceOiData(binanceTokens, binanceTokensCoin, timeframe)
             // combineData()
             // setLoading(false)
             localStorage.setItem("timeframe", JSON.stringify(timeframe))
@@ -61,8 +90,10 @@ export default function OpenInterestAll() {
         setBinanceOiCombined([]);
         combineData();
         localStorage.setItem("tokenBool", JSON.stringify(weights));
+        localStorage.setItem("tokenCoinBool", JSON.stringify(weights));
         // setLoading(false);
-    }, [weights, loading])
+    }, [weights, weightsCoin, loading])
+
 
     function combineData() {
         const combList = binanceOi.map((tokenData, index) => {
@@ -85,12 +116,19 @@ export default function OpenInterestAll() {
         // setLoading(false);
     }
 
-    function getBinanceOiData(tokens, timeframe) {
-        const url = "https://fapi.binance.com/fapi/v1/exchangeInfo";
-        axios.get(url).then(response => {
+    function getBinanceOiData(tokens, tokensCoin, timeframe) {
+        axios.get("https://fapi.binance.com/fapi/v1/exchangeInfo").then(response => {
                 // console.log(response.data)
                 response.data.symbols.map((tokenData, index) => {
                     setBinanceTokens(oldData => [...oldData, tokenData.symbol])
+                })
+            }
+        )
+        axios.get("https://dapi.binance.com/dapi/v1/exchangeInfo").then(response => {
+                // console.log(response.data)
+                response.data.symbols.map((tokenData, index) => {
+                    setBinanceTokensCoin(oldData => [...oldData, tokenData.symbol]);
+                    setBinanceTokensCoinType(oldData => [...oldData, tokenData.contractType]);
                 })
             }
         )
@@ -101,10 +139,25 @@ export default function OpenInterestAll() {
                             setBinanceOi(oldData => [...oldData, results])
                         }
                     )
-                setLoading(false);
+                    // setLoading(false);
                 initWeights(binanceTokens);
-                combineData();
                 }
+            )
+        const prefixCoin = "https://dapi.binance.com/futures/data/openInterestHist?pair=";
+        Promise.all(tokensCoin.map((u, i) => axios.get(prefixCoin + u.split("_")[0] + "&period=" + timeframe + "&limit=500&contractType=" + binanceTokensCoinType[i])))
+            .then(responses => {
+                console.log(responses)
+                    responses.map((results, index) => {
+                        console.log(results)
+                            setBinanceOiCoin(oldData => [...oldData, results])
+                        }
+                    )
+                    setLoading(false);
+
+                    initWeightsCoin(binanceTokensCoin);
+                    combineData();
+                }
+
             )
     }
 
@@ -117,7 +170,6 @@ export default function OpenInterestAll() {
                 })
             }
         )
-        // console.log(binanceTokens)
     }
 
     return (
@@ -140,33 +192,80 @@ export default function OpenInterestAll() {
                         </div>
                         <OpenInterestChart chartData={binanceOiCombined} chartHeight={420} chartTitle=""/>
                         <div className="flex flex-row gap-1 justify-center">
-                            <button className={`${timeframe === '4h' ? " " : "text-gray-300 dark:text-gray-700 "}` + "w-12 rounded-lg"}
-                                    onClick={() => setTimeframe('4h')}>4h
+                            <button
+                                className={`${timeframe === '4h' ? " " : "text-gray-300 dark:text-gray-700 "}` + "w-12 rounded-lg"}
+                                onClick={() => setTimeframe('4h')}>4h
                             </button>
-                            <button className={`${timeframe === '1h' ? " " : "text-gray-300 dark:text-gray-700 "}` + "w-12 rounded-lg"}
-                                    onClick={() => setTimeframe('1h')}>1h
+                            <button
+                                className={`${timeframe === '1h' ? " " : "text-gray-300 dark:text-gray-700 "}` + "w-12 rounded-lg"}
+                                onClick={() => setTimeframe('1h')}>1h
                             </button>
-                            <button className={`${timeframe === '15m' ? " " : "text-gray-300 dark:text-gray-700 "}` + "w-12 rounded-lg"}
-                                    onClick={() => setTimeframe('15m')}>15m
+                            <button
+                                className={`${timeframe === '15m' ? " " : "text-gray-300 dark:text-gray-700 "}` + "w-12 rounded-lg"}
+                                onClick={() => setTimeframe('15m')}>15m
                             </button>
                         </div>
-                        <div
-                            className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-12 gap-2 text-xs justify-center px-4 text-center mt-4">
-                            {binanceTokens.map((token, index) => (
-                                <div
-                                    key={token}
-                                    className="items-center text-center">
-                                    {/*<p>{token}</p>*/}
-                                    {/*<input type="number"*/}
-                                    {/*       onChange={(e) => updateWeights(index)}*/}
-                                    {/*       defaultValue={weights[index]}*/}
-                                    {/*       className="px-3 py-1 w-20 bg-white dark:bg-custom-gray-a"/>*/}
-                                    <button className={`${weights[index] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
+                        <div>
+                            <h1 className="">binance usd-margined futures</h1>
+                            <div
+                                className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-12 gap-2 text-xs justify-center px-4 text-center mt-4">
+                                {binanceTokens.map((token, index) => (
+                                    <div
+                                        key={token}
+                                        className="items-center text-center">
+                                        {/*<p>{token}</p>*/}
+                                        {/*<input type="number"*/}
+                                        {/*       onChange={(e) => updateWeights(index)}*/}
+                                        {/*       defaultValue={weights[index]}*/}
+                                        {/*       className="px-3 py-1 w-20 bg-white dark:bg-custom-gray-a"/>*/}
+                                        <button
+                                            className={`${weights[index] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
                                             onClick={() => updateWeights(index)}>{token}
-                                    </button>
-                                </div>
-                            ))}
+                                        </button>
+                                    </div>
+                                ))}
 
+                            </div>
+                            <h1 className="mt-4">binance coin-margined futures</h1>
+                            <div
+                                className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-12 gap-2 text-xs justify-center px-4 text-center mt-4">
+                                {binanceTokensCoin.map((token, index) => (
+                                    <div
+                                        key={token}
+                                        className="items-center text-center">
+                                        {/*<p>{token}</p>*/}
+                                        {/*<input type="number"*/}
+                                        {/*       onChange={(e) => updateWeights(index)}*/}
+                                        {/*       defaultValue={weights[index]}*/}
+                                        {/*       className="px-3 py-1 w-20 bg-white dark:bg-custom-gray-a"/>*/}
+                                        <button
+                                            className={`${weightsCoin[index] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
+                                            onClick={() => updateWeightsCoin(index)}>{token}
+                                        </button>
+                                    </div>
+                                ))}
+
+                            </div>
+                            {/*<h1 className="mt-4">ftx futures</h1>*/}
+                            {/*<div*/}
+                            {/*    className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-8 lg:grid-cols-12 gap-2 text-xs justify-center px-4 text-center mt-4">*/}
+                            {/*    {binanceTokensCoin.map((token, index) => (*/}
+                            {/*        <div*/}
+                            {/*            key={token}*/}
+                            {/*            className="items-center text-center">*/}
+                            {/*            /!*<p>{token}</p>*!/*/}
+                            {/*            /!*<input type="number"*!/*/}
+                            {/*            /!*       onChange={(e) => updateWeights(index)}*!/*/}
+                            {/*            /!*       defaultValue={weights[index]}*!/*/}
+                            {/*            /!*       className="px-3 py-1 w-20 bg-white dark:bg-custom-gray-a"/>*!/*/}
+                            {/*            <button*/}
+                            {/*                className={`${weightsCoin[index] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}*/}
+                            {/*                onClick={() => updateWeightsCoin(index)}>{token}*/}
+                            {/*            </button>*/}
+                            {/*        </div>*/}
+                            {/*    ))}*/}
+
+                            {/*</div>*/}
                         </div>
                     </div>
                     <div
