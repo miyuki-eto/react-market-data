e1ee905938e288cc45b6c136d8fc1927579537b4
Miyuki Eto <82160202+miyuki-eto@users.noreply.github.com>
2021-11-13 19:19:32 +1000

    nance update

--- src/pages/openInterestAll.js
+++ src/pages/openInterestAll.js
@@ -9,13 +9,16 @@ axiosThrottle.use(axios, {requestsPerSecond: 9});
 export default function OpenInterestAll() {
     const [binanceTokens, setBinanceTokens] = useState([]);
     const [binanceTokensCoin, setBinanceTokensCoin] = useState([]);
-    const [binanceTokensCoinType, setBinanceTokensCoinType] = useState([]);
-    const [weights, setWeights] = useState(() => {
-        return JSON.parse(localStorage.getItem("tokenBool")) || [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    // const [binanceTokensCoinType, setBinanceTokensCoinType] = useState([]);
+    // const [weights, setWeights] = useState(() => {
+    //     return JSON.parse(localStorage.getItem("tokenBool")) || [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    // });
+    const [tokenDict, setTokenDict] = useState(() => {
+        return JSON.parse(localStorage.getItem("tokenDict")) || {};
     });
-    const [weightsCoin, setWeightsCoin] = useState(() => {
-        return JSON.parse(localStorage.getItem("tokenCoinBool")) || [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-    });
+    // const [weightsCoin, setWeightsCoin] = useState(() => {
+    //     return JSON.parse(localStorage.getItem("tokenCoinBool")) || [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    // });
 
     const [binanceOi, setBinanceOi] = useState([]);
     const [binanceOiCoin, setBinanceOiCoin] = useState([]);
@@ -25,43 +28,57 @@ export default function OpenInterestAll() {
         return JSON.parse(localStorage.getItem("timeframe")) || "1h";
     });
 
-    function updateWeights(index) {
-        let weightsCopy = [...weights];
-        if (weightsCopy[index] === 1) {
-            weightsCopy[index] = 0;
+    // function updateWeights(index) {
+    //     let weightsCopy = [...weights];
+    //     if (weightsCopy[index] === 1) {
+    //         weightsCopy[index] = 0;
+    //     } else {
+    //         weightsCopy[index] = 1;
+    //     }
+    //     setWeights(weightsCopy);
+    // }
+
+    function updateDict(token) {
+        // console.log(token)
+        const tokenObj = tokenDict;
+        if (tokenObj[token] === 1) {
+            tokenObj[token] = 0;
         } else {
-            weightsCopy[index] = 1;
+            tokenObj[token] = 1;
         }
-        setWeights(weightsCopy);
+        // console.log(tokenObj)
+        setTokenDict(tokenObj);
+        localStorage.setItem("tokenDict", JSON.stringify(tokenObj));
+        // console.log(tokenDict)
     }
 
-    function updateWeightsCoin(index) {
-        let weightsCopy = [...weightsCoin];
-        if (weightsCopy[index] === 1) {
-            weightsCopy[index] = 0;
-        } else {
-            weightsCopy[index] = 1;
-        }
-        setWeightsCoin(weightsCopy);
-    }
-
-    function initWeights(tokens) {
-        const arr = [];
-        const len = tokens.length;
-        for (let i = 1; i <= len; i++) {
-            arr.push(1);
-        }
-        setWeights(arr)
-    }
-
-    function initWeightsCoin(tokens) {
-        const arr = [];
-        const len = tokens.length;
-        for (let i = 1; i <= len; i++) {
-            arr.push(1);
-        }
-        setWeightsCoin(arr)
-    }
+    // function updateWeightsCoin(index) {
+    //     let weightsCopy = [...weightsCoin];
+    //     if (weightsCopy[index] === 1) {
+    //         weightsCopy[index] = 0;
+    //     } else {
+    //         weightsCopy[index] = 1;
+    //     }
+    //     setWeightsCoin(weightsCopy);
+    // }
+    //
+    // function initWeights(tokens) {
+    //     const arr = [];
+    //     const len = tokens.length;
+    //     for (let i = 1; i <= len; i++) {
+    //         arr.push(1);
+    //     }
+    //     setWeights(arr)
+    // }
+    //
+    // function initWeightsCoin(tokens) {
+    //     const arr = [];
+    //     const len = tokens.length;
+    //     for (let i = 1; i <= len; i++) {
+    //         arr.push(1);
+    //     }
+    //     setWeightsCoin(arr)
+    // }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -70,23 +87,25 @@ export default function OpenInterestAll() {
             setBinanceOiCoin([])
             setBinanceTokens([])
             setBinanceTokensCoin([])
-            setBinanceTokensCoinType([])
+            // setBinanceTokensCoinType([])
             const usdTokens = await getBinanceTokens();
             setBinanceTokens(usdTokens)
 
             const coinTokens = await getBinanceTokensCoin();
             setBinanceTokensCoin(coinTokens)
-            initWeights(usdTokens);
-            initWeightsCoin(coinTokens);
+            await updateTokenDict(usdTokens)
+            await updateTokenDict(coinTokens)
+            // initWeights(usdTokens);
+            // initWeightsCoin(coinTokens);
             const usdData = await getBinanceOi(usdTokens);
             setBinanceOi(usdData)
             const coinData = await getBinanceOiCoin(coinTokens);
             setBinanceOiCoin(coinData)
             setBinanceOi(usdData.concat(coinData))
-            console.log(usdData.concat(coinData))
+            // console.log(usdData.concat(coinData))
             // stackData();
             // console.log(usdData)
-            const mergedData = await mergeData(usdData.concat(coinData), weights.concat(weightsCoin));
+            const mergedData = await mergeData(usdData.concat(coinData));
             setBinanceOiCombined(mergedData)
             setLoading(false);
             // getBinanceOiData(binanceTokens, binanceTokensCoin, timeframe)
@@ -102,26 +121,42 @@ export default function OpenInterestAll() {
     useEffect(() => {
         const updateData = async () => {
             setBinanceOiCombined([]);
-            const mergedData = await mergeData(binanceOi.concat(binanceOiCoin), weights.concat(weightsCoin));
+            // await updateTokenDict(binanceTokens)
+            // await updateTokenDict(binanceTokensCoin)
+            const mergedData = await mergeData(binanceOi.concat(binanceOiCoin));
             setBinanceOiCombined(mergedData)
-            localStorage.setItem("tokenBool", JSON.stringify(weights));
-            localStorage.setItem("tokenCoinBool", JSON.stringify(weightsCoin));
+            // localStorage.setItem("tokenBool", JSON.stringify(weights));
+            // localStorage.setItem("tokenCoinBool", JSON.stringify(weightsCoin));
             // setLoading(false);
         }
         updateData();
-    }, [weights, weightsCoin, loading])
+    }, [loading, tokenDict])
 
 
-    async function mergeData(data, weight) {
+    async function updateTokenDict(tokenList) {
+        const tokenObj = tokenDict;
+        await tokenList.forEach((x) => {
+            if (!(x in tokenObj)) {
+                tokenObj[x] = 1;
+            }
+        })
+        setTokenDict(tokenObj)
+        localStorage.setItem("tokenDict", JSON.stringify(tokenObj));
+    }
+
+
+    async function mergeData(data) {
         const r = {};
-        console.log(weight)
-        console.log(data.length)
+        // console.log(weight)
+        // console.log(data.length)
         await data.forEach((o, i) => {
             // console.log(weight[i])
             // console.log(o.data)
             o.data.forEach(function (x){
                 // console.log(x)
-                r[x.timestamp] = (r[x.timestamp] || 0) + (parseFloat(x.sumOpenInterestValue) * weight[i]);
+                // const symbolName = ('symbol' in x) ? x.symbol : x.pair;
+                // console.log(symbolName)
+                r[x.timestamp] = (r[x.timestamp] || 0) + (parseFloat(x.sumOpenInterestValue) * tokenDict[('symbol' in x) ? x.symbol : x.pair]);
             })
 
         })
@@ -176,7 +211,7 @@ export default function OpenInterestAll() {
     async function getBinanceOiCoin(tokens) {
         const data = [];
         const prefix = "https://dapi.binance.com/futures/data/openInterestHist?pair=";
-        await Promise.all(tokens.map((u, i) => axios.get(prefix + u.split("_")[0] + "&period=" + timeframe + "&limit=500&contractType=" + binanceTokensCoinType[i])))
+        await Promise.all(tokens.map((u, i) => axios.get(prefix + u.split("_")[0] + "&period=" + timeframe + "&limit=500&contractType=ALL" )))
             .then(responses => {
                     responses.map((results) => {
                             data.push(results)
@@ -221,8 +256,8 @@ export default function OpenInterestAll() {
                                         key={token}
                                         className="items-center text-center">
                                         <button
-                                            className={`${weights[index] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
-                                            onClick={() => updateWeights(index)}>{token}
+                                            className={`${tokenDict[token] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
+                                            onClick={() => updateDict(token)}>{token}
                                         </button>
                                     </div>
                                 ))}
@@ -236,8 +271,8 @@ export default function OpenInterestAll() {
                                         key={token}
                                         className="items-center text-center">
                                         <button
-                                            className={`${weights[index + binanceTokens.length] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
-                                            onClick={() => updateWeights(index + binanceTokens.length)}>{token}
+                                            className={`${tokenDict[token] === 1 ? " " : "text-gray-300 dark:text-gray-700 "}` + "rounded-lg"}
+                                            onClick={() => updateDict(token)}>{token}
                                         </button>
                                     </div>
                                 ))}
